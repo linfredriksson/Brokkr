e30fb09775a67a58f74924f85e1999aadda313fb
Linus Fredriksson <linfredriksson@gmail.com>
2015-10-24 01:50:32 +0200

    - Add load function that runs when program is starting. - Add update function that is run for every tick. - Add simple test code for drawing a rectangle and choosing draw color.

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,13 @@
+function love.load()
+end
+
+function love.update()
+end
+
 function love.draw()
+	love.graphics.setColor(255, 100, 100)
+	love.graphics.rectangle("fill", 10, 10, 10, 10)
+
+	love.graphics.setColor(255, 255, 255)
     love.graphics.print("Hello World", 400, 300)
 end
