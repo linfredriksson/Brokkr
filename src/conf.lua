5230724b5b6f644867b368c39d558e563adf104d
Linus Fredriksson <linfredriksson@gmail.com>
2015-10-25 20:03:25 +0100

    - Turn on console. - Add very simple collision check between walls and player.

--- src/conf.lua
+++ src/conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
-    t.version = "0.9.2"                -- The LÖVE version this game was made for (string)
-    t.console = false                  -- Attach a console (boolean, Windows only)
- 
+    t.version = "0.9.2"                -- The LÃ–VE version this game was made for (string)
+    t.console = true                  -- Attach a console (boolean, Windows only)
+
     t.window.title = "Brokkr"        -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
     t.window.width = 768               -- The window width (number)
@@ -20,7 +20,7 @@ function love.conf(t)
     t.window.srgb = false              -- Enable sRGB gamma correction when drawing to the screen (boolean)
     t.window.x = nil                   -- The x-coordinate of the window's position in the specified display (number)
     t.window.y = nil                   -- The y-coordinate of the window's position in the specified display (number)
- 
+
     t.modules.audio = true             -- Enable the audio module (boolean)
     t.modules.event = true             -- Enable the event module (boolean)
     t.modules.graphics = true          -- Enable the graphics module (boolean)
--- src/main.lua
+++ src/main.lua
@@ -1,18 +1,18 @@
 local anim8 = require "anim8"
 
-function love.load()	
-	tileset = love.graphics.newImage("img/example_tiles.png")	
+function love.load()
+	tileset = love.graphics.newImage("img/example_tiles.png")
 	local tilesetWidth = tileset:getWidth();
 	local tilesetHeight = tileset:getHeight();
 	tileWidth = 64
 	tileHeight = 64
-	
+
 	tiles = {}
-	tiles[0] = love.graphics.newQuad(0,                        0, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
-	tiles[1] = love.graphics.newQuad(tileWidth,             0, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
-	tiles[2] = love.graphics.newQuad(0,            tileHeight, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
-	tiles[3] = love.graphics.newQuad(tileWidth, tileHeight, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
-	
+	tiles[0] = love.graphics.newQuad(0,										0, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
+	tiles[1] = love.graphics.newQuad(tileWidth,						0, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
+	tiles[2] = love.graphics.newQuad(0,					tileHeight, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
+	tiles[3] = love.graphics.newQuad(tileWidth,	tileHeight, tileWidth, tileHeight, tilesetWidth, tilesetHeight)
+
 	map = {
 		{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
 		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
@@ -23,14 +23,14 @@ function love.load()
 		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
 		{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
 	}
-	
+
 	player = {
 		x = 80, y = 80, direction = 0, speed = 100,
 		keyUp = "up", keyDown = "down", keyRight = "right", keyLeft = "left",
 		spritesheet = love.graphics.newImage("img/characters1.png"),
 		animation = {}
 	}
-	
+
 	local grid = anim8.newGrid(32, 32, player.spritesheet:getWidth(), player.spritesheet:getHeight())
 	player.animation[0] = anim8.newAnimation(grid("1-3", 1), 0.2)
 	player.animation[1] = anim8.newAnimation(grid("1-3", 4), 0.2)
@@ -50,11 +50,16 @@ function love.update(dt)
 	else
 		player.animation[player.direction]:gotoFrame(2)
 	end
-	
-	if love.keyboard.isDown(player.keyUp)		then player.direction = 1; player.y = player.y - dt * player.speed end
-	if love.keyboard.isDown(player.keyDown)	then player.direction = 0; player.y = player.y + dt * player.speed end
-	if love.keyboard.isDown(player.keyLeft)	then player.direction = 3; player.x = player.x - dt * player.speed end
-	if love.keyboard.isDown(player.keyRight)	then player.direction = 2; player.x = player.x + dt * player.speed end
+
+	local x = player.x
+	local y = player.y
+	if love.keyboard.isDown(player.keyUp)		then player.direction = 1; y = player.y - dt * player.speed end
+	if love.keyboard.isDown(player.keyDown)	then player.direction = 0; y = player.y + dt * player.speed end
+	if love.keyboard.isDown(player.keyLeft)	then player.direction = 3; x = player.x - dt * player.speed end
+	if love.keyboard.isDown(player.keyRight)then player.direction = 2; x = player.x + dt * player.speed end
+	local intX = math.floor((x + 16) / love.graphics.getWidth() * (12))
+	local intY = math.floor((y + 16) / love.graphics.getHeight() * (8))
+	if map[intY + 1][intX + 1] == 0 then player.x = x; player.y = y end
 end
 
 function love.draw()
@@ -68,7 +73,7 @@ function love.draw()
 
 	-- draw player
 	player.animation[player.direction]:draw(player.spritesheet, player.x, player.y)
-	
+
 	-- print hello world
-    love.graphics.print("Hello World", love.graphics.getWidth() * 0.5 - 35, love.graphics.getHeight() * 0.5 - 5)
+  love.graphics.print("Hello World", love.graphics.getWidth() * 0.5 - 35, love.graphics.getHeight() * 0.5 - 5)
 end
