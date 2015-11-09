ad026c89de0a3b55435fde0c7d6115ff9ef236ef
Linus Fredriksson <linfredriksson@gmail.com>
2015-11-09 02:33:40 +0100

    - Add code to setup love-connection.

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,18 @@
+local Net = require "Net"
+
+local windowWidth = 0
+local windowHeight = 0
+
 function love.load()
+  windowWidth = love.graphics.getWidth()
+  windowHeight = love.graphics.getHeight()
+
+  Net:init("Server")
+  Net:connect(nil, 6789)
 end
 
 function love.update(dt)
+  Net:update(dt)
 end
 
 function love.draw()
