5db6bc108bda61ec7b9469e5303cf1738bd1cc5e
Linus Fredriksson <linfredriksson@gmail.com>
2015-11-09 02:19:41 +0100

    - Update window names of client and server.

--- client/conf.lua
+++ client/conf.lua
@@ -1,9 +1,9 @@
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
     t.version = "0.9.2"                -- The LÖVE version this game was made for (string)
-    t.console = true                  -- Attach a console (boolean, Windows only)
+    t.console = true                   -- Attach a console (boolean, Windows only)
 
-    t.window.title = "Brokkr"        -- The window title (string)
+    t.window.title = "Brokkr - Client" -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
     t.window.width = 768               -- The window width (number)
     t.window.height = 512              -- The window height (number)
--- server/conf.lua
+++ server/conf.lua
@@ -1,9 +1,9 @@
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
     t.version = "0.9.2"                -- The LÖVE version this game was made for (string)
-    t.console = true                  -- Attach a console (boolean, Windows only)
+    t.console = true                   -- Attach a console (boolean, Windows only)
 
-    t.window.title = "Brokkr Server"        -- The window title (string)
+    t.window.title = "Brokkr - Server" -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
     t.window.width = 200               -- The window width (number)
     t.window.height = 100              -- The window height (number)
