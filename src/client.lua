3a5db010b5b223748d2618a48c7215243e6dfb18
Linus Fredriksson <linfredriksson@gmail.com>
2015-11-19 00:25:28 +0100

    - Add server/client code to server.lua and client.lua. Taken from previous server/client main.lua files. - Add Net.lua, networking library. - Add anim8.lua, animation library. - Add test images and sound.

--- src/client.lua
+++ src/client.lua
@@ -1,23 +1,170 @@
+local anim8 = require "dependencies/anim8"
+local Net = require "dependencies/Net"
 local client = {}
 
 client.load = function(self)
-  print("client")
+  self.players = {}
+  self.worldTileGrid = nil
+  self.worldTileWidth = 64
+  self.worldTileHeight = 64
+  self.characterTileWidth = 32
+  self.characterTileHeight = 32
+  self.ip, self.port = "127.0.0.1", 6789
+  self.maxPing = 1000
+
+  Net:init("client")
+	Net:connect(self.ip, self.port)
+	Net:setMaxPing(self.maxPing)
+
+	Net:registerCMD("showLocation",
+		function(table, param, dt, id)
+			table["Param"] = nil
+			table["Command"] = nil
+
+			for k, v in pairs(table) do
+				if self.players[k] == nil then -- initiate if not done already
+					self.players[k] = {}
+					self.players[k].animation = {
+						anim8.newAnimation(self.worldTileGrid("1-3", 1), 0.2),
+						anim8.newAnimation(self.worldTileGrid("1-3", 4), 0.2),
+						anim8.newAnimation(self.worldTileGrid("1-3", 3), 0.2),
+						anim8.newAnimation(self.worldTileGrid("1-3", 2), 0.2)
+					}
+				end
+
+				-- player is still in the network
+				self.players[k].alive = true
+				self.players[k].x, self.players[k].y, self.players[k].direction, self.players[k].isMoving = v:match("^(%-?[%d.e]*),(%-?[%d.e]*),(%-?[%d.e]*),(%-?[%d.e]*)$")
+			end
+
+			for k, v in pairs(self.players) do
+				if self.players[k].alive == false then
+					self.players[k] = nil
+				else
+					self.players[k].alive = false
+				end
+			end
+
+		end)
+
+	self.sound = love.audio.newSource("sound/footstep01.ogg")
+	self.tileset = love.graphics.newImage("image/example_tiles.png")
+
+	local tilesetWidth = self.tileset:getWidth();
+	local tilesetHeight = self.tileset:getHeight();
+
+	self.tiles = {
+		{walkable = true, destructable = false, img = love.graphics.newQuad(0, 0, self.worldTileWidth, self.worldTileHeight, tilesetWidth, tilesetHeight)},
+		{walkable = true, destructable = false, img = love.graphics.newQuad(self.worldTileWidth, 0, self.worldTileWidth, self.worldTileHeight, tilesetWidth, tilesetHeight)},
+		{walkable = true, destructable = false, img = love.graphics.newQuad(0, self.worldTileHeight, self.worldTileWidth, self.worldTileHeight, tilesetWidth, tilesetHeight)},
+		{walkable = true, destructable = false, img = love.graphics.newQuad(self.worldTileWidth,	self.worldTileHeight, self.worldTileWidth, self.worldTileHeight, tilesetWidth, tilesetHeight)}
+	}
+
+	self.map = {
+		{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 0, 3, 0, 0, 0, 0, 0, 0, 3, 0, 2},
+		{2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+		{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+	}
+
+	self.player = {
+		x = love.graphics.getWidth() * 0.5 - self.characterTileWidth * 0.5, y = love.graphics.getHeight() * 0.5 - self.characterTileHeight * 0.5,
+		direction = 1, speed = 100,
+		keyUp = "up", keyDown = "down", keyRight = "right", keyLeft = "left",
+		spritesheet = love.graphics.newImage("image/characters1.png"),
+		animation = {}
+	}
+
+	self.worldTileGrid = anim8.newGrid(self.characterTileWidth, self.characterTileHeight, self.player.spritesheet:getWidth(), self.player.spritesheet:getHeight())
+	self.player.animation = {
+		anim8.newAnimation(self.worldTileGrid("1-3", 1), 0.2),
+		anim8.newAnimation(self.worldTileGrid("1-3", 4), 0.2),
+		anim8.newAnimation(self.worldTileGrid("1-3", 3), 0.2),
+		anim8.newAnimation(self.worldTileGrid("1-3", 2), 0.2)
+	}
 end
 
 client.mousepressed = function(self, x, y, button)
 end
 
 client.keypressed = function(self, key)
+  if key == "escape" then
+		Net:disconnect()
+		love.event.quit()
+	end
+
+	if key == "w" then
+		love.audio.play(self.sound)
+	end
+
+	if key == "up" or key == "down" or key == "right" or key == "left" then
+		Net:send({}, "key_pressed", key, Net.client.ip)
+		--print("key_pressed: " .. key)
+	end
 end
 
 client.keyreleased = function(self, key)
+  if key == "up" or key == "down" or key == "right" or key == "left" then
+		Net:send({}, "key_released", key, Net.client.ip)
+		--print("key_released: " .. key)
+	end
 end
 
 client.update = function(self, dt)
+  for k, v in pairs(self.players) do
+		if v.isMoving == "1" then
+			v.animation[tonumber(v.direction)]:update(dt)
+			--sound:play()
+		else
+			v.animation[tonumber(v.direction)]:gotoFrame(2)
+		end
+	end
+
+	if love.keyboard.isDown(self.player.keyUp, self.player.keyDown, self.player.keyRight, self.player.keyLeft) then
+		self.player.animation[self.player.direction]:update(dt)
+		--sound:play()
+	else
+		self.player.animation[self.player.direction]:gotoFrame(2)
+	end
+
+	local x = self.player.x
+	local y = self.player.y
+	if love.keyboard.isDown(self.player.keyUp)   then self.player.direction = 2; y = y - dt * self.player.speed end
+	if love.keyboard.isDown(self.player.keyDown)	then self.player.direction = 1; y = y + dt * self.player.speed end
+	if love.keyboard.isDown(self.player.keyLeft)	then self.player.direction = 4; x = x - dt * self.player.speed end
+	if love.keyboard.isDown(self.player.keyRight)then self.player.direction = 3; x = x + dt * self.player.speed end
+	--local intX = math.floor((x + 16) / love.graphics.getWidth() * (12))
+	--local intY = math.floor((y + 16) / love.graphics.getHeight() * (8))
+	--if map[intY + 1][intX + 1] == 0 then player.x = x; player.y = y end
+	self.player.x = x
+	self.player.y = y
+
+	Net:update(dt)
 end
 
 client.draw = function(self)
-  love.graphics.print("client", 10, 10)
+  -- draw map
+	for y = 1, #self.map  do
+		local row = self.map[y]
+		for x = 1, #row do
+			love.graphics.draw(self.tileset, self.tiles[self.map[y][x] + 1].img, x * self.worldTileWidth - self.worldTileWidth, y * self.worldTileHeight - self.worldTileHeight)
+		end
+	end
+
+	-- draw all players
+	for k, v in pairs(self.players) do
+		v.animation[tonumber(v.direction)]:draw(self.player.spritesheet, v.x, v.y)
+	end
+
+	-- draw player
+	self.player.animation[self.player.direction]:draw(self.player.spritesheet, self.player.x, self.player.y)
+
+	-- print hello world
+	--love.graphics.print("Hello World", love.graphics.getWidth() * 0.5 - 35, love.graphics.getHeight() * 0.5 - 5)
 end
 
 return client
--- src/server.lua
+++ src/server.lua
@@ -1,10 +1,33 @@
+local Net = require "dependencies/Net"
 local server = {}
 
 server.load = function(self)
-  print("server")
+  self.windowWidth = 768 -- client window size
+  self.windowHeight = 512 -- client window size
+  self.worldTileWidth = 64
+  self.worldTileHeight = 64
+  self.characterTileWidth = 32
+  self.characterTileHeight = 32
+  self.ip, self.port = nil, 6789
+  self.maxPing = 3000
+  self.totalDeltaTime = 0
+  self.updateTimeStep = 0.01
+
+  Net:init("Server")
+  Net:connect(self.ip, self.port)
+  Net:setMaxPing(self.maxPing)
+
+  Net:registerCMD("key_pressed", function(table, param, id) self:keyPressed(id, param, true) end)
+  Net:registerCMD("key_released", function(table, param, id) self:keyPressed(id, param, false) end)
 end
 
 server.mousepressed = function(self, x, y, button)
+end
+
+server.keyPressed = function(self, id, key, value)
+  if Net.users[id] and (key == "up" or key == "down" or key == "right" or key == "left") then
+    Net.users[id].key[key] = value
+  end
 end
 
 server.keypressed = function(self, key)
@@ -14,6 +37,47 @@ server.keyreleased = function(self, key)
 end
 
 server.update = function(self, dt)
+  self.totalDeltaTime = self.totalDeltaTime + dt
+  while self.totalDeltaTime > self.updateTimeStep do
+    self:fixedUpdate(self.updateTimeStep)
+    self.totalDeltaTime = self.totalDeltaTime - self.updateTimeStep
+  end
+end
+
+server.fixedUpdate = function(self, dt)
+  Net:update(dt)
+
+  local clients = {}
+
+  for id, data in pairs(Net:connectedUsers()) do
+    if data.greeted ~= true then
+      Net:send({}, "print", "Welcome to Brokkr", id)
+      data.greeted = true
+      Net.users[id].x = self.windowWidth * 0.5 - self.characterTileWidth * 0.5
+      Net.users[id].y = self.windowHeight * 0.5 - self.characterTileHeight * 0.5
+      Net.users[id].speed = 100
+      Net.users[id].direction = 1
+      Net.users[id].isMoving = 0
+      Net.users[id].key = {}
+    end
+
+    local change = dt * Net.users[id].speed
+    if Net.users[id].key["up"] then Net.users[id].direction = 2; Net.users[id].y = Net.users[id].y - change end
+    if Net.users[id].key["down"] then Net.users[id].direction = 1; Net.users[id].y = Net.users[id].y + change end
+    if Net.users[id].key["right"] then Net.users[id].direction = 3; Net.users[id].x = Net.users[id].x + change end
+    if Net.users[id].key["left"] then Net.users[id].direction = 4; Net.users[id].x = Net.users[id].x - change end
+
+    Net.users[id].isMoving = 0
+    if Net.users[id].key["up"] or Net.users[id].key["down"] or Net.users[id].key["right"] or Net.users[id].key["left"] then
+      Net.users[id].isMoving = 1
+    end
+
+    clients[id] = Net.users[id].x .. "," .. Net.users[id].y .. "," .. Net.users[id].direction .. "," .. Net.users[id].isMoving
+  end
+
+  for id, data in pairs(Net:connectedUsers()) do
+    Net:send(clients, "showLocation", "", id)
+  end
 end
 
 server.draw = function(self)
