f863f1e77b65ab08f754b6ed4bc1b80752200f76
Linus Fredriksson <linfredriksson@gmail.com>
2015-11-21 20:55:34 +0100

    - Add function to create a map full of walls. - Rename local variable to not be the same as global variable with same name since both is used in the same function Map:chooseMap().

--- src/map.lua
+++ src/map.lua
@@ -1,8 +1,9 @@
 local map = {}
 
 function map:chooseMap(mapName, worldTileWidth, worldTileHeight, width, height)
-	local map = nil
-	if mapName == "empty" then map = emptyMap(width, height)
+	local m = nil
+	if mapName == "empty" then m = map:emptyMap(width, height)
+	elseif mapName == "full" then m = map:fullMap(width, height)
 	else error("There is no such a map called \""..mapName.."\"!")
 	end
 
@@ -17,23 +18,49 @@ function map:chooseMap(mapName, worldTileWidth, worldTileHeight, width, height)
 		{walkable = true, destructable = false, img = love.graphics.newQuad(worldTileWidth, worldTileHeight, worldTileWidth, worldTileHeight, tilesetWidth, tilesetHeight)}
 	}
 
-	return tileset, tiles, map
+	return tileset, tiles, m
 end
 
-function emptyMap(width, height)
+--[[ Returns a empty map with only walls around the border. ]]
+map.emptyMap = function (self, width, height)
 	local m = {}
 	local wall = 2
 	local floor = 0
+
 	for y = 1, height do
 		m[y]= {}
 		for x = 1, width do m[y][x] = floor end
 		m[y][width] = wall
 		m[y][1] = wall
 	end
+
 	for i = 1, width do
 		m[height][i] = wall
 		m[1][i] = wall
 	end
+
+	return m
+end
+
+--[[ Returns a full map with only empty spaces where the characters start. ]]
+map.fullMap = function(self, width, height)
+	local m = {}
+	local wall = 2
+	local floor = 0
+
+	for y = 1, height do
+		m[y] = {}
+		for x = 1, width do m[y][x] = wall end
+	end
+
+	local offset = {{0, 0}, {0, 1}, {1, 0}, {1, 1}}
+	for i = 1, 4 do
+		m[offset[i][1] + 2][offset[i][2] + 2] = floor
+		m[offset[i][1] + 2][-offset[i][2] + width - 1] = floor
+		m[-offset[i][1] + height - 1][offset[i][2] + 2] = floor
+		m[-offset[i][1] + height - 1][-offset[i][2] + width - 1] = floor
+	end
+
 	return m
 end
 
