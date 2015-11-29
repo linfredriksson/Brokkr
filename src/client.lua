7778761d08c5edfdf1e4b6126a89becb1b479cb5
Deniz Kennedy <dekespo@gmail.com>
2015-11-29 22:30:56 +0200

    Moving the creation map from the function update to the cmd register

--- src/client.lua
+++ src/client.lua
@@ -12,7 +12,7 @@ client.load = function(self)
 	self.characterTile = {grid = nil, width = 32, height = 32}
 	self.world = {tileWidth = 32, tileHeight = 32, width = 24, height= 16}
 	self.ip, self.port, self.maxPing = "127.0.0.1", 6789, 1000
-	self.map = {name = "empty"} -- empety is the default value
+	self.map = {name = "empty"} -- empty is the default value
 
 	-- Define keys for different actions
 	self.actions = {up = "up", down = "down", left = "left", right = "right", bomb = " "}
@@ -28,7 +28,9 @@ client.load = function(self)
 	Net:registerCMD("getMapName",
 		function(table, param, dt, id)
 			self.map.name = table["map"]
-			self.map.received = true
+			for k, v in pairs(Map:chooseMap(self.map.name, self.world)) do
+				self.map[k] = v
+			end
 		end)
 
 	Net:registerCMD("showLocation",
@@ -242,20 +244,14 @@ client.update = function(self, dt)
 
 	Net:update(dt)
 
-	-- Update the map
-	if self.map.received then
-		for k, v in pairs(Map:chooseMap(self.map.name, self.world)) do
-			self.map[k] = v
-		end
-		self.map.received = false -- Server sends the message once anyway
-	end
 end
 
 client.draw = function(self)
 	-- draw map
 	for y = 1, #self.map.values  do
 		for x = 1, #self.map.values[y] do
-			love.graphics.draw(self.map.tileset, self.map.tiles[self.map.values[y][x] + 1].img, x * self.world.tileWidth - self.world.tileWidth, y * self.world.tileHeight - self.world.tileHeight)
+			--love.graphics.draw(self.map.tileset, self.map.tiles[self.map.values[y][x]].img, x * self.world.tileWidth - self.world.tileWidth, y * self.world.tileHeight - self.world.tileHeight)
+			love.graphics.draw(self.map.tileset, self.map.tiles[self.map.values[y][x]].img, (x - 1) * self.world.tileWidth, (y - 1) * self.world.tileHeight)
 		end
 	end
 
