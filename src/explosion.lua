4e6e1bf860d92cc605bb16b3bd8baa0eeaa1e4e0
Linus Fredriksson <linfredriksson@gmail.com>
2015-12-07 00:39:17 +0100

    - Add comments for functions in explosion.lua.

--- src/explosion.lua
+++ src/explosion.lua
@@ -1,10 +1,19 @@
 local explosion = {}
 
+--[[
+	Initiate the explosion instances and type lists.
+]]
 explosion.initiate = function(self)
 	self.instances = {}
 	self.type = {}
 end
 
+--[[
+	Creates a object that can be used to render an explosion.
+	- inImage: is a loaded image containing the tileset of a explosion animation.
+	- inNumberOfTiles: is the number of tiles in the tileset image.
+	- inAnimationDuration: is the total animation time of the explosion.
+]]
 explosion.addType = function(self, inImage, inNumberOfTiles, inAnimationDuration)
 	local type = {
 		animationDuration = inAnimationDuration,
@@ -24,12 +33,21 @@ explosion.addType = function(self, inImage, inNumberOfTiles, inAnimationDuration
 	self.type[#self.type + 1] = type
 end
 
+--[[
+
+]]
 explosion.addInstance = function(self, type, directions, posX, posY, spreadDistance, spreadRate)
 end
 
+--[[
+
+]]
 explosion.updateAnimation = function(self, dt)
 end
 
+--[[
+
+]]
 explosion.update = function(self, dt)
 end
 
