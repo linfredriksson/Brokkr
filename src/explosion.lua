dac1bdd869dcb32b5d28e2bc0a7557552f3f8511
Linus Fredriksson <linfredriksson@gmail.com>
2015-12-07 17:54:07 +0100

    - Fix bug where wrong variable name was used in explosion.addType(). - Remove explosion type from explosion.addInstance() argument list. Instead assign random type inside the function. - Implement explosion.update(). - Add explosion.spread(). Called from explosion.update() and is used to create new instances whenever a existing instance is spreading.

--- src/explosion.lua
+++ src/explosion.lua
@@ -1,3 +1,5 @@
+local anim8 = require "dependencies/anim8"
+
 local explosion = {}
 
 --[[
@@ -6,6 +8,7 @@ local explosion = {}
 explosion.initiate = function(self)
 	self.instances = {}
 	self.type = {}
+
 end
 
 --[[
@@ -21,7 +24,7 @@ explosion.addType = function(self, inImage, inNumberOfTiles, inAnimationDuration
 		numberOfTiles = inNumberOfTiles,
 		tileset = inImage,
 		tileWidth = math.floor(inImage:getWidth() / inNumberOfTiles),
-		tileHeight = image:getHeight(),
+		tileHeight = inImage:getHeight(),
 		grid = nil
 	}
 	type.grid = anim8.newGrid(
@@ -35,14 +38,14 @@ end
 
 --[[
 	Creates a explosion instance on the map square newX, newY.
-	- inType: is the explosion type.
 	- inDirections: is used to show in wich directions the explosion will spread.
 	- inPosX: is the x coordinate in the self.map.
 	- inPosY: is the y coordinate in the selt.map.
 	- inSpreadDistance: indicates how far the explision will spread from its center.
 	- inSpreadRate: indicates how fast the explosion will spread.
 ]]
-explosion.addInstance = function(self, inType, inDirections, inPosX, inPosY, inSpreadDistance, inSpreadRate)
+explosion.addInstance = function(self, inDirections, inPosX, inPosY, inSpreadDistance, inSpreadRate)
+	local inType = self.type[math.random(#self.type)] -- assign random explosion type
 	local instance = {
 		type = inType,
 		timer = inType.animationDuration,
@@ -67,9 +70,83 @@ explosion.updateAnimation = function(self, dt)
 end
 
 --[[
-
+	Updates all explosion instances currently on the map and spread new ones if needed.
+	- dt: delta time since last update.
 ]]
-explosion.update = function(self, dt)
+explosion.update = function(self, inMap, dt)
+	local instances = self.instances
+
+	-- empty the instance list, then add back the instances that still
+	-- have time left on its timer.
+	self.instances = {}
+
+	-- update all instances in the local instance table
+	for instanceID = 1, #instances do
+		local instance = instances[instanceID]
+		instance.timer = instance.timer - dt
+
+		if instance.spreadDistance > 0 and instance.timer < instance.spreadRate then
+			self:spread(instance, inMap, dt)
+			instance.spreadDistance = 0
+		end
+
+		if instance.timer > 0 then
+			self.instances[#self.instances + 1] = instance
+		end
+	end
+end
+
+--[[
+	Spread a instance if needed.
+	- instance: instance of a explosion.
+	- dt: delta time since last update.
+]]
+explosion.spread = function(self, instance, inMap, dt)
+	local walkable = {
+		current = true,
+		above = inMap.tiles[inMap.values[instance.y + 0][instance.x + 1] + 1].walkable,
+		below = inMap.tiles[inMap.values[instance.y + 2][instance.x + 1] + 1].walkable
+	}
+	local offsetX = {0, 1, 0, -1}
+	local offsetY = {-1, 0, 1, 0}
+
+	for dir1ID = 1, #instance.spreadDirections do
+		local dir1 = instance.spreadDirections[dir1ID]
+		local directions = {}
+		local pos = {
+			x = instance.x + offsetX[dir1],
+			y = instance.y + offsetY[dir1]
+		}
+
+		walkable.current = inMap.tiles[inMap.values[pos.y + 1][pos.x + 1] + 1].walkable
+
+		if walkable.current then
+			-- find wich directions the explosion will continue to spread in
+			for dir2ID = 1, #instance.spreadDirections do
+				local dir2 = instance.spreadDirections[dir2ID]
+
+				if (dir1 == 2 and dir2 ~= 4) or
+					(dir1 == 4 and dir2 ~= 2) or
+					(dir1 == dir2)
+				then
+					if (dir2 == 2) or (dir2 == 4) or
+						(dir2 == 1 and walkable.above) or
+						(dir2 == 3 and walkable.below)
+					then
+						directions[#directions + 1] = dir2
+					end
+				end
+			end
+
+			self:addInstance(
+				directions,
+				pos.x,
+				pos.y,
+				instance.spreadDistance - 1,
+				instance.spreadRate
+			)
+		end
+	end
 end
 
 return explosion
