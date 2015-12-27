d58ee1ad4019aea5d26eed1bb4320589f0bf2145
Linus Fredriksson <linfredriksson@gmail.com>
2015-12-27 18:14:20 +0100

    - Change window title to "Brokkr" instead of "Brokkr - Menu".

--- src/conf.lua
+++ src/conf.lua
@@ -3,7 +3,7 @@ function love.conf(t)
 	t.version = "0.10.0"                -- The LÖVE version this game was made for (string)
 	t.console = true                   -- Attach a console (boolean, Windows only)
 
-	t.window.title = "Brokkr - Menu" -- The window title (string)
+	t.window.title = "Brokkr"          -- The window title (string)
 	t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
 	t.window.width = 768               -- The window width (number)
 	t.window.height = 512              -- The window height (number)
