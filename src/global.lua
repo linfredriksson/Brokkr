785f445d08c3f17356d26a5f37b791811ea60fca
Linus Fredriksson <linfredriksson@gmail.com>
2016-03-31 16:39:59 +0200

    - Add global.lua containing default variables used by both client.lua and server.lua. Before these variables were defined in both files instead of in only one place.

---
+++ src/global.lua
@@ -0,0 +1,44 @@
+local global = {
+	actions = {
+		up = "up",       -- move up
+		down = "down",   -- move down
+		left = "left",   -- move left
+		right = "right", -- move right
+		bomb = "space",  -- place bomb
+		prev = "a",      -- choose previous character, while in lobby
+		next = "s"       -- choose next character, while in lobby
+	},
+	window = {
+		width = love.graphics.getWidth(),
+		height = love.graphics.getHeight()
+	},
+	characterTile = {
+		imageName = "image/characters1.png",
+		sprite = nil,
+		grid = nil,
+		width = 32,
+		height = 32,
+		numberOfCharacters = 7
+	},
+	map = {
+		tileImageName = "image/tiles.png",
+		tileWidth = 32,
+		tileHeight = 32,
+		mapWidth = 24,
+		mapHeight = 16
+	},
+	gameMap = {
+		map = "random", -- type of map
+		seed = os.time() -- seed used in math.random() when generating map
+	},
+	lobbyMap = {
+		map = "lobby",
+		seed = 0
+	},
+	ip = "127.0.0.1",
+	port = 6789,
+	maxPingServer = 3000,
+	maxPingClient = 1000 -- ping time before it pings the server again
+}
+
+return global
