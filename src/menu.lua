00eb29aa9f7559b2ff9f9a6cf5a16e603eac02c3
Linus Fredriksson <linfredriksson@gmail.com>
2016-04-20 17:29:34 +0200

    - Add option for changing server ip before trying to connect to a server. Before ip was hardcoded.

--- src/menu.lua
+++ src/menu.lua
@@ -1,3 +1,4 @@
+local Global = require "global"
 local menu = {}
 
 --[[
@@ -7,6 +8,10 @@ menu.load = function(self)
 	menu.originalFont = love.graphics.getFont()
 	menu.font = love.graphics.newFont(50)
 	love.graphics.setFont(menu.font)
+	self.ip = Global.ip
+	self.invalid = {r = 255, g = 0, b = 0, a = 255} -- color for when self.ip is not a valid ip adress
+	self.valid = {r = 0, g = 255, b = 0, a = 255} -- color for when self.ip is a valid ip adress
+	self.ipColor = self.valid
 end
 
 --[[
@@ -16,17 +21,26 @@ end
 	- button: which button on the mouse was pressed.
 ]]
 menu.mousepressed = function(self, x, y, button)
-	love.graphics.setFont(menu.originalFont)
+	local start = false
 
-	-- start server or client depending which half of the screen the user clicks on.
+	-- server
 	if y < love.graphics.getHeight() * 0.5 then
 		run = require "server"
-	else
-		run = require "client"
+		start = true
 	end
 
-	-- initiate client or server.
-	run:load()
+	-- client
+	if y >= love.graphics.getHeight() * 0.5 and self.ipColor == self.valid then
+		run = require "client"
+		Global.ip = self.ip
+		start = true
+	end
+
+	-- start server or client depending which half of the screen the user clicks on.
+	if start then
+		love.graphics.setFont(menu.originalFont)
+		run:load()
+	end
 end
 
 --[[
@@ -34,6 +48,11 @@ end
 	- key: keyboard button beeing pressed.
 ]]
 menu.keypressed = function(self, key)
+	if key == "backspace" then self.ip = string.sub(self.ip, 0, -2) end
+	local char = key:gsub("kp", "")
+	if tonumber(char) or char == "." then self.ip = self.ip .. char end
+	self.ipColor = self.invalid
+	if self:validIp(self.ip) then self.ipColor = self.valid end
 end
 
 --[[
@@ -55,7 +74,6 @@ end
 	bottom half of window is client button.
 ]]
 menu.draw = function(self)
-	love.graphics.setColor(255, 255, 255, 255)
 	love.graphics.rectangle("fill", 0, 0, 1000, 1000)
 	love.graphics.setColor(0, 0, 0, 255)
 	love.graphics.rectangle("fill", 0, love.graphics.getHeight() * 0.5, 1000, 1000)
@@ -63,12 +81,30 @@ menu.draw = function(self)
 	love.graphics.print("HOST GAME", 10, love.graphics.getHeight() * 0.5 - 55)
 	love.graphics.setColor(255, 255, 255, 255)
 	love.graphics.print("JOIN GAME", 10, love.graphics.getHeight() * 0.5 + 5)
+	love.graphics.setColor(self.ipColor.r, self.ipColor.g, self.ipColor.b, self.ipColor.a)
+	love.graphics.print("IP: " .. self.ip, 10, love.graphics.getHeight() - menu.font:getHeight())
+	love.graphics.setColor(255, 255, 255, 255)
 end
 
 --[[
 	Quit function.
 ]]
 menu.quit = function(self)
+end
+
+--[[
+	Check if a certain ip is a valid ip4 adress with four sequences of numbers
+	in the range of 0 to 999 separated by dots. Returns true if it is valid,
+	otherwise false.
+	- ip: a string containing the ip to check.
+]]
+menu.validIp = function(self, ip)
+	local tmp = {self.ip:match("^(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)$")}
+	for k, v in pairs(tmp) do if tonumber(v) > 255 then return false end end
+	if #tmp == 4 then
+		return true
+	end
+	return false
 end
 
 return menu
