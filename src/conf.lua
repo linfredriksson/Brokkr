1402e166738029568abcfcbd3ca48d03683482ef
Linus Fredriksson <linfredriksson@gmail.com>
2016-04-24 23:00:44 +0200

    - Update start menu.

--- src/conf.lua
+++ src/conf.lua
@@ -1,10 +1,10 @@
 function love.conf(t)
 	t.identity = nil                   -- The name of the save directory (string)
 	t.version = "0.10.0"               -- The LÖVE version this game was made for (string)
-	t.console = true                   -- Attach a console (boolean, Windows only)
+	t.console = false                  -- Attach a console (boolean, Windows only)
 
 	t.window.title = "Brokkr"          -- The window title (string)
-	t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
+	t.window.icon = "image/bomb.png"   -- Filepath to an image to use as the window's icon (string)
 	t.window.width = 768               -- The window width (number)
 	t.window.height = 512              -- The window height (number)
 	t.window.borderless = false        -- Remove all border visuals from the window (boolean)
--- src/menu.lua
+++ src/menu.lua
@@ -5,13 +5,22 @@ local menu = {}
 	Initialising function.
 ]]
 menu.load = function(self)
-	menu.originalFont = love.graphics.getFont()
-	menu.font = love.graphics.newFont(50)
-	love.graphics.setFont(menu.font)
+	self.originalFont = love.graphics.getFont()
 	self.ip = Global.ip
 	self.invalid = {r = 255, g = 0, b = 0, a = 255} -- color for when self.ip is not a valid ip adress
 	self.valid = {r = 0, g = 255, b = 0, a = 255} -- color for when self.ip is a valid ip adress
 	self.ipColor = self.valid
+	self.headerFont = love.graphics.newFont("font/Ash.ttf", 70)
+	self.buttonFont = love.graphics.newFont("font/Ash.ttf", 30)
+	self.ipFont = love.graphics.newFont("font/Ash.ttf", 20)
+	self.backgroundImage = love.graphics.newImage("image/background.png")
+
+	-- initiate gui
+	self.guiElements = {}
+	self:addGuiItem("BROKKR", menu.headerFont, {r = 0, g = 0, b = 0, a = 255}, {r = 0, g = 0, b = 0, a = 0}, 10, 200, Global.window.width - 20, menu.headerFont:getHeight() - 15, 0)
+	self:addGuiItem("HOST SERVER", menu.buttonFont, {r = 144, g = 144, b = 144, a = 255}, {r = 0, g = 0, b = 0, a = 255}, 10, 10, Global.window.width - 40, menu.buttonFont:getHeight(), 10)
+	self:addGuiItem("JOIN SERVER", menu.buttonFont, {r = 144, g = 144, b = 144, a = 255}, {r = 0, g = 0, b = 0, a = 255}, 10, 10, Global.window.width - 40, menu.buttonFont:getHeight(), 10)
+	self:addGuiItem("IP: 127.0.0.1", menu.ipFont, self.valid, {r = 0, g = 0, b = 0, a = 0}, 10, 10, Global.window.width - 20, menu.ipFont:getHeight(), 0)
 end
 
 --[[
@@ -23,14 +32,16 @@ end
 menu.mousepressed = function(self, x, y, button)
 	local start = false
 
-	-- server
-	if y < love.graphics.getHeight() * 0.5 then
+	-- check to see if server button is pressed
+	local b1 = self.guiElements[2]
+	if self:overButton(x, y, b1.x, b1.y, b1.width, b1.height) then
 		run = require "server"
 		start = true
 	end
 
-	-- client
-	if y >= love.graphics.getHeight() * 0.5 and self.ipColor == self.valid then
+	-- check to see if client button is pressed
+	local b2 = self.guiElements[3]
+	if self:overButton(x, y, b2.x, b2.y, b2.width, b2.height) and self.ipColor == self.valid then
 		run = require "client"
 		Global.ip = self.ip
 		start = true
@@ -53,6 +64,8 @@ menu.keypressed = function(self, key)
 	if tonumber(char) or char == "." then self.ip = self.ip .. char end
 	self.ipColor = self.invalid
 	if self:validIp(self.ip) then self.ipColor = self.valid end
+	self.guiElements[4].color = self.ipColor
+	self.guiElements[4].text = "IP: " .. self.ip
 end
 
 --[[
@@ -74,15 +87,15 @@ end
 	bottom half of window is client button.
 ]]
 menu.draw = function(self)
-	love.graphics.rectangle("fill", 0, 0, 1000, 1000)
-	love.graphics.setColor(0, 0, 0, 255)
-	love.graphics.rectangle("fill", 0, love.graphics.getHeight() * 0.5, 1000, 1000)
-	love.graphics.setColor(0, 0, 0, 255)
-	love.graphics.print("HOST GAME", 10, love.graphics.getHeight() * 0.5 - 55)
-	love.graphics.setColor(255, 255, 255, 255)
-	love.graphics.print("JOIN GAME", 10, love.graphics.getHeight() * 0.5 + 5)
-	love.graphics.setColor(self.ipColor.r, self.ipColor.g, self.ipColor.b, self.ipColor.a)
-	love.graphics.print("IP: " .. self.ip, 10, love.graphics.getHeight() - menu.font:getHeight())
+	love.graphics.draw(self.backgroundImage, 0, 0)
+	--love.graphics.rectangle("fill", 0, 0, Global.window.width, Global.window.height)
+	for k, v in pairs(self.guiElements) do
+		love.graphics.setFont(v.font)
+		love.graphics.setColor(v.bgColor.r, v.bgColor.g, v.bgColor.b, v.bgColor.a)
+		love.graphics.rectangle("fill", v.x, v.y, v.width, v.height)
+		love.graphics.setColor(v.color.r, v.color.g, v.color.b, v.color.a)
+		love.graphics.print(v.text, v.x + v.padding, v.y + v.padding)
+	end
 	love.graphics.setColor(255, 255, 255, 255)
 end
 
@@ -99,11 +112,54 @@ end
 	- ip: a string containing the ip to check.
 ]]
 menu.validIp = function(self, ip)
-	local tmp = {self.ip:match("^(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)$")}
+	local tmp = {ip:match("^(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)$")}
 	for k, v in pairs(tmp) do if tonumber(v) > 255 then return false end end
-	if #tmp == 4 then
-		return true
-	end
+	if #tmp == 4 then return true end
+	return false
+end
+
+--[[
+	Add a new element to the gui.
+	- text: text contained whitin the gui element.
+	- font: font used to display the text.
+	- color: font color.
+	- bgColor: color of the element box.
+	- xOffset: horizontal offset from the left side of the window.
+	- yOffset: vertical offset from the previous element.
+	- width: width of the element box.
+	- height: height of the element box.
+	- padding: padding between the box edge and the containing text.
+]]
+menu.addGuiItem = function(self, text, font, color, bgColor, xOffset, yOffset, width, height, padding)
+	local previousOffsetY = 0
+	local lastElement = self.guiElements[#self.guiElements]
+	-- offset it with respect of the previous gui element
+	if #self.guiElements > 0 then previousOffsetY = lastElement.y + lastElement.height end
+	local item = {
+		text = text,
+		font = font,
+		color = color,
+		bgColor = bgColor,
+		x = xOffset,
+		y = previousOffsetY + yOffset,
+		width = width + padding * 2,
+		height = height + padding * 2,
+		padding = padding
+	}
+	table.insert(self.guiElements, item)
+end
+
+--[[
+	Check if the mouse is ontop of a gui element.
+	- mouseX: mouse x coordinate.
+	- mouseY: mouse y coordinate.
+	- boxX: x coordinate of upper left corner of the button.
+	- boxY: y coordinate of upper left corner of the button.
+	- width: width of the button.
+	- height: height of the button.
+]]
+menu.overButton = function(self, mouseX, mouseY, boxX, boxY, width, height)
+	if mouseX > boxX and mouseX < boxX + width and mouseY > boxY and mouseY < boxY + height then return true end
 	return false
 end
 
